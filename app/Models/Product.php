7c96d7a7d4f6872b2b17dd9522ae6aa8d5a4a8f0
muhammadali <mohammadali.drg@gmail.com>
2020-10-28 16:39:45 +0330

    relations

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    public function color()
+    {
+        return $this->belongsTo(Color::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function productcount()
+    {
+        return $this->hasOne(ProductCount::class);
+    }
+}
---
+++ database/migrations/2020_10_28_114818_create_product_counts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_counts', function (Blueprint $table) {
+            $table->foreignId('product_id')->constrained();
+            $table->unsignedBigInteger('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_counts');
+    }
+}
